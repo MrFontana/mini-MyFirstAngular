221df9c8c8df05ce783c05e5ebc5ba8f4b7f1004
Kevin Fontana <kheartse@yahoo.com>
2016-09-26 12:18:51 -0600

    finished project

---
+++ js/mainCtrl.js
@@ -0,0 +1,19 @@
+angular.module('friendsList').controller('mainCtrl', function($scope) {
+  $scope.friends = [{
+      name: "Joe",
+      age: "27 or 28ish"
+  },
+  {
+    name: "Joe number 2",
+    age: 29
+  },
+  {
+    name: "Brayden",
+    age: 22
+  },
+  {
+    name: "Brett muhFuggin Caudill",
+    age: 28
+  }
+]
+});
